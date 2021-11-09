576da53320c5c94261fe23fa601e903987107720
trieule50 <trieule50@gmail.com>
2021-11-09 14:18:29 -0500

    Remove styling sheet

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,9 +3,9 @@ import { Container } from "react-bootstrap"
 const Home = () => {
     return(
         <Container>
-            <div>
+            <div className='landing-img'>
                 <h1>Welcome to WanderList</h1>
-                <img src="https://image.flaticon.com/icons/png/512/5169/5169182.png" alt="car traveling" style={{height: '25rem'}}/>
+                {/* <img src="https://image.flaticon.com/icons/png/512/5169/5169182.png" alt="car traveling" style={{height: '25rem'}}/> */}
                 <h5>Come and view recommended places to see in each state!</h5>
             </div>
         </Container>
