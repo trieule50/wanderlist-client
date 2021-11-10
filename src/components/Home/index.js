4c507ed22fee42e39e68317095266d880782fdcd
trieule50 <trieule50@gmail.com>
2021-11-10 08:41:02 -0500

    Add landing page photo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,11 @@
-import { Container } from "react-bootstrap"
-
 const Home = () => {
     return(
-        <Container>
             <div className='landing-img'>
+                <div className='landing-text'>
                 <h1>Welcome to WanderList</h1>
-                {/* <img src="https://image.flaticon.com/icons/png/512/5169/5169182.png" alt="car traveling" style={{height: '25rem'}}/> */}
-                <h5>Come and view recommended places to see in each state!</h5>
+                <small>Come and view recommended places to see in each state!</small>
+                </div>
             </div>
-        </Container>
     )
 }
 
